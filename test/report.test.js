680eaf19f3ccccd95cf739c46a52674f566a46a5
M. Faisal Rahman <faizal.btg@gmail.com>
2016-11-28 15:56:23 +0700

    added test for GET /api/report/:report_id

--- test/report.test.js
+++ test/report.test.js
@@ -56,4 +56,30 @@ describe("Sample unit testing for report collection", function() {
                 done();
             })
     });
+
+    it("should return array of object", function(done) {
+        server
+            .get("/api/report/58394a4f2d2d8802086ee989")
+            .set('Accept', 'application/json')
+            .expect(200)
+            .end(function(error, result) {
+                if (error) done(error);
+                should.not.exist(error);
+                let response = result.body;
+
+                response.should.be.an('object');
+                expect(response).to.have.property('title');
+                expect(response).to.have.property('description');
+                expect(response).to.have.property('latitude');
+                expect(response).to.have.property('longitude');
+                expect(response).to.have.property('timestamp');
+
+                assert.isString(response.title);
+                assert.isString(response.description);
+                assert.isNumber(response.latitude);
+                assert.isNumber(response.longitude);
+
+                done();
+            })
+    });
 });
