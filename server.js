d1a646c22ec1e17e35aaaba35c33b892474ef272
M. Faisal Rahman <faizal.btg@gmail.com>
2017-01-09 15:16:20 +0700

    update

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ let router      = express.Router();
 let report      = require('./app/controllers/reportController');
 let port = process.env.PORT || 5000;
 
+//test
 mongoose.connect('mongodb://188.166.246.129:27017/express-api-mongodb');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
